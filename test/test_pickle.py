151e41c0a62d05addc8e23f6236d89a54da985b3
Evan Sultanik <evan.sultanik@trailofbits.com>
2022-09-11 14:05:10 -0400

    Adds tests to reproduce the missing cleanup for injected code

--- test/test_pickle.py
+++ test/test_pickle.py
@@ -66,6 +66,13 @@ class TestInterpreter(TestCase):
         self.assertIsInstance(loaded[-1], fpickle.Stop)
         loaded.insert_python_eval("[5, 6, 7, 8]", use_output_as_unpickle_result=True)
         self.assertIsInstance(loaded[-1], fpickle.Stop)
+
+        # Make sure the injected code cleans up the stack after itself:
+        interpreter = Interpreter(loaded)
+        interpreter.run()
+        self.assertEqual(len(interpreter.stack), 0)
+
+        # Make sure the output is correct
         evaluated = loads(loaded.dumps())
         self.assertEqual([5, 6, 7, 8], evaluated)
 
@@ -75,6 +82,13 @@ class TestInterpreter(TestCase):
         self.assertIsInstance(loaded[-1], fpickle.Stop)
         loaded.insert_python_eval("[5, 6, 7, 8]", run_first=False, use_output_as_unpickle_result=False)
         self.assertIsInstance(loaded[-1], fpickle.Stop)
+
+        # Make sure the injected code cleans up the stack after itself:
+        interpreter = Interpreter(loaded)
+        interpreter.run()
+        self.assertEqual(len(interpreter.stack), 0)
+
+        # Make sure the output is correct
         evaluated = loads(loaded.dumps())
         self.assertEqual([1, 2, 3, 4], evaluated)
 
