498910b75ab6503231cd2bac5e3f84d7be290146
suhacker1 <suhashussain1@gmail.com>
2023-09-21 22:49:30 -0400

    Add tearDown to tests

--- test/test_pytorch.py
+++ test/test_pytorch.py
@@ -2,6 +2,7 @@ import torchvision.models as models
 import torch 
 from fickling.pytorch import PyTorchModelWrapper
 from unittest import TestCase
+import os
 
 
 class PyTorchTestCase(TestCase):
@@ -11,11 +12,19 @@ class PyTorchTestCase(TestCase):
         torch.save(self.model, self.file_path)
         deserialized_model = torch.load('mobilenet_v2.pt')
         self.wrapper = PyTorchModelWrapper('mobilenet_v2.pt')
+        self.output_path = 'altered_model.pt'
 
     def test_torch_loading(self):
         self.assertIsInstance(self.wrapper, PyTorchModelWrapper)
 
     def test_fallback_injection(self):
         payload = '''print("Hello, World!")'''
-        self.wrapper.inject_payload(payload, 'altered_model.pt')
-        torch.load('altered_model.pt')
+        self.wrapper.inject_payload(payload, self.output_path, 
+                                    injection="fallback")
+        torch.load(self.output_path)
+    
+    def tearDown(self):
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
+        if os.path.exists(self.output_path):
+            os.remove(self.output_path)
