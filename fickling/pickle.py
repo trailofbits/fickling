e06664586d2a91ae58c3c8d171bd0de1a7ac2e21
Evan Sultanik <evan.sultanik@trailofbits.com>
2023-11-20 11:00:17 -0500

    Appease ruff/linting

--- fickling/pickle.py
+++ fickling/pickle.py
@@ -1,6 +1,6 @@
 import warnings
 
-from fickling.fickle import *
+from fickling.fickle import *  # noqa: F403
 
 warnings.warn("As of version 0.0.8, `fickling.pickle` has been renamed `fickling.fickle`. "
               "The `fickling.pickle` module will be removed in a future release!",
