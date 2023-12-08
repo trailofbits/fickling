7e8ed8efdfd6a1377d84b03388a806b7222a0dc9
Suha Sabi Hussain <suha.hussain@trailofbits.com>
2023-12-08 12:04:30 -0500

    Refactor

--- fickling/pytorch.py
+++ fickling/pytorch.py
@@ -29,51 +29,55 @@ class PyTorchModelWrapper:
         self._pickled: Optional[Pickled] = None
         self.force: bool = force
 
-    @property
-    def pickled(self) -> Pickled:
-        if self._pickled is None:
-            formats = fickling.polyglot.identify_pytorch_file_format(self.path)
-            # One option was to raise an error if PyTorch v1.3 was not found or if any of the TorchScript versions were found.
-            # However, that would prevent polyglots from being loaded.
-            # Therefore, the 'force' argument was created to enable users to do that while loading different warnings.
-            # Another option was to only include the warning if "Pytorch v1.3" was not the most likely format.
-            # Instead, the file formats are directly specified for clarity and independence.
-            if len(formats) == 0:
+    def validate_file_format(self):
+        formats = fickling.polyglot.identify_pytorch_file_format(self.path)
+        # One option was to raise an error if PyTorch v1.3 was not found or if any of the TorchScript versions were found.
+        # However, that would prevent polyglots from being loaded.
+        # Therefore, the 'force' argument was created to enable users to do that while loading different warnings.
+        # Another option was to only include the warning if "PyTorch v1.3" was not the most likely format.
+        # Instead, the file formats are directly specified for clarity and independence.
+        if len(formats) == 0:
+            if self.force == True:
+                warnings.warn(
+                    "This file has not been identified as a PyTorch file. If it is a PyTorch file, raise an issue on GitHub",
+                    UserWarning,
+                )
+            else:
+                raise ValueError(
+                    "This file has not been identified as a PyTorch file. If it is a PyTorch file, raise an issue on GitHub."
+                )
+        if ("PyTorch v1.3" not in formats) or {
+            "TorchScript v1.4",
+            "TorchScript v1.3",
+            "TorchScript v1.1",
+            "TorchScript v1.0",
+        }.intersection(formats):
+            if "PyTorch v0.1.10" in formats:
                 if self.force == True:
                     warnings.warn(
-                        "This file has not been identified as a PyTorch file. If it is a PyTorch file, raise an issue on GitHub",
+                        "This file may be a PyTorch v0.1.10 file. Try Pickled.load() or StackedPickle.load() if this fails",
                         UserWarning,
                     )
                 else:
                     raise ValueError(
-                        "This file has not been identified as a PyTorch file. If it is a PyTorch file, raise an issue on GitHub."
+                        "This file may be a PyTorch v0.1.10 file. Try Pickled.load() or StackedPickle.load() instead or use the argument `force=True`."
                     )
-            if ("PyTorch v1.3" not in formats) or {
-                "TorchScript v1.4",
-                "TorchScript v1.3",
-                "TorchScript v1.1",
-                "TorchScript v1.0",
-            }.intersection(formats):
-                if "PyTorch v0.1.10" in formats:
-                    if self.force == True:
-                        warnings.warn(
-                            "This file may be a PyTorch v0.1.10 file. Try Pickled.load() or StackedPickle.load() if this fails",
-                            UserWarning,
-                        )
-                    else:
-                        raise ValueError(
-                            "This file may be a PyTorch v0.1.10 file. Try Pickled.load() or StackedPickle.load() instead or use the argument `force=True`."
-                        )
+            else:
+                if self.force == True:
+                    warnings.warn(
+                        "A fickling wrapper and injection method has not been developed for that format. Please raise an issue on our GitHub.",
+                        UserWarning,
+                    )
                 else:
-                    if self.force == True:
-                        warnings.warn(
-                            "A fickling wrapper and injection method may not have been developed for that format. Please raise an issue on our GitHub.",
-                            UserWarning,
-                        )
-                    else:
-                        raise NotImplementedError(
-                            "A fickling wrapper and injection method may not have been for that format. Please raise an issue on our GitHub or use the argument `force=True`."
-                        )
+                    raise NotImplementedError(
+                        "A fickling wrapper and injection method has not been for that format. Please raise an issue on our GitHub or use the argument `force=True`."
+                    )
+        return
+
+    @property
+    def pickled(self) -> Pickled:
+        if self._pickled is None:
+            self.validate_file_format()
             with zipfile.ZipFile(self.path, "r") as zip_ref:
                 data_pkl_path = next(
                     (name for name in zip_ref.namelist() if name.endswith("/data.pkl")), None
