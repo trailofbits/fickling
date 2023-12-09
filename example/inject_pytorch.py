1523e1ffecd308bca51f076e02ca7c31cce78cf0
Suha Sabi Hussain <suha.hussain@trailofbits.com>
2023-12-09 02:30:45 -0500

    Add PyTorch module example

---
+++ example/inject_pytorch.py
@@ -0,0 +1,20 @@
+import torch
+import torchvision.models as models
+
+from fickling.pytorch import PyTorchModelWrapper
+
+# Load example PyTorch model
+model = models.mobilenet_v2()
+torch.save(model, "mobilenet.pth")
+
+# Wrap model file into fickling
+result = PyTorchModelWrapper("mobilenet.pth")
+
+# Inject payload, overwriting the existing file instead of creating a new one
+temp_filename = "temp_filename.pt"
+result.inject_payload(
+    "print('Hello, World!')", temp_filename, injection="insertion", overwrite=True
+)
+
+# Load file with injected payload
+torch.load("mobilenet.pth")
