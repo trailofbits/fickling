be2488f3d2c1dba13c2f97ce5ff128dc727f8988
Suha Sabi Hussain <suha.hussain@trailofbits.com>
2023-12-15 12:42:35 -0500

    Fix experiment files

--- example/torch_exp_one.py
+++ example/torch_exp_one.py
@@ -1,9 +1,8 @@
 import torch
 import torchvision.models as models
-
+import fickling.hook as hook
 # Set up global fickling hook
 
-"""
 hook.run_hook()
 model = models.mobilenet_v2()
 
@@ -14,13 +13,4 @@ print("MODEL")
 torch.load("model.pt")
 print("LEGACY MODEL")
 torch.load("legacy_model.pt")
-"""
-from fickling.pytorch import PyTorchModelWrapper
 
-model = models.mobilenet_v2()
-torch.save(model, "model.pt")
-result = PyTorchModelWrapper("model.pt")
-# print(dir(result))
-pkl = result.pickled
-print(dir(pkl))
-result = pkl.check_safety.severity
