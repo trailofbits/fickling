329513e44d72bb8599a1821ace714cf82478043f
Thomas Chauchefoin <thomas.chauchefoin@trailofbits.com>
2025-12-02 11:58:41 +0100

    Use BytesIO in tests whenever possible

--- test/test_hook.py
+++ test/test_hook.py
@@ -1,10 +1,9 @@
-import os
+import io
 import pickle
 import unittest
 from pickle import UnpicklingError
 
 import numpy
-import torch
 
 import fickling.hook as hook
 from fickling.exception import UnsafeFileError
@@ -16,6 +15,8 @@ class Payload:
         self.a = 1
 
     def __reduce__(self):
+        import os
+
         return (os.system, ("echo 'I should have been stopped by the hook'",))
 
 
@@ -27,37 +28,19 @@ class TestHook(unittest.TestCase):
     def test_safe_pickle(self):
         # Fickling can check a pickle file for safety prior to running it
         test_list = [1, 2, 3]
-
-        # Create "safe" pickle file
-        with open("safe.pkl", "wb") as file:
-            pickle.dump(test_list, file)
-
-        # Load the safe pickle file
-        with open("safe.pkl", "rb") as file:
-            loaded_data = pickle.load(file)
-
-        if os.path.exists("safe.pkl"):
-            os.remove("safe.pkl")
-
-        # Assert that the loaded data matches the original data
+        data = pickle.dumps(test_list)
+        loaded_data = pickle.load(io.BytesIO(data))
         self.assertEqual(loaded_data, test_list)
 
     def test_unsafe_pickle(self):
-        # Create "unsafe" pickle file
-
+        # Create "unsafe" pickle bytes
         payload = Payload()
-
-        # Save the payload in a pickle file
-        with open("unsafe.pickle", "wb") as f:
-            pickle.dump(payload, f)
+        data = pickle.dumps(payload)
 
         try:
-            numpy.load("unsafe.pickle", allow_pickle=True)
+            numpy.load(io.BytesIO(data), allow_pickle=True)
         except UnpicklingError as e:
             if isinstance(e.__cause__, UnsafeFileError):
                 pass
             else:
                 self.fail(e)
-
-        if os.path.exists("unsafe.pickle"):
-            os.remove("unsafe.pickle")
