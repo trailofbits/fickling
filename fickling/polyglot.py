d9b3c43da3fdfbb017bcb8748572638e44cb5695
Thomas Chauchefoin <thomas.chauchefoin@trailofbits.com>
2026-01-17 01:13:52 +0100

    Use random temporary files when extracting archives in find_file_properties_recursively()

--- fickling/polyglot.py
+++ fickling/polyglot.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import stat
 import tarfile
 import tempfile
 import zipfile
@@ -181,30 +182,35 @@ def find_file_properties_recursively(file_path, print_properties=False):
     # actually check the zip
     if check_zip:
         properties["children"] = {}
-        with tempfile.TemporaryDirectory() as tempdir:
-            with zipfile.ZipFile(file_path) as zipped_file:
-                for fname in zipped_file.namelist():
-                    zipped_file.extract(fname, path=tempdir)
-                    fname_path = os.path.join(tempdir, fname)
-                    properties["children"][fname] = find_file_properties_recursively(
-                        fname_path, print_properties
-                    )
+        with zipfile.ZipFile(file_path) as zipped_file:
+            for fname in zipped_file.namelist():
+                info = zipped_file.getinfo(fname)
+                mode = info.external_attr >> 16
+                if info.is_dir():
+                    continue
+                if mode == 0 or stat.S_ISREG(mode):
+                    with tempfile.NamedTemporaryFile() as _tempfile:
+                        _tempfile.write(zipped_file.read(fname))
+                        _tempfile.flush()
+                        properties["children"][fname] = find_file_properties_recursively(
+                            _tempfile.name, print_properties
+                        )
 
     # check tar
     if properties["is_tar"]:  # tar archive
         properties["children"] = {}
-        with tempfile.TemporaryDirectory() as tempdir:
-            with tarfile.TarFile(file_path) as tarred_file:
-                for fname in tarred_file.getnames():
-                    content = tarred_file.extractfile(fname)
-                    if content is None:
-                        properties["children"][fname] = None
-                        continue
-                    fname_path = os.path.join(tempdir, os.path.basename(fname))
-                    open(fname_path, "wb").write(content.read())
-                    properties["children"][fname] = find_file_properties_recursively(
-                        fname_path, print_properties
-                    )
+        with tarfile.TarFile(file_path) as tarred_file:
+            for fname in tarred_file.getmembers():
+                if fname.isfile():
+                    with tempfile.NamedTemporaryFile() as _tempfile:
+                        if not (content := tarred_file.extractfile(fname)):
+                            properties["children"][fname.name] = None
+                            continue
+                        _tempfile.write(content.read())
+                        _tempfile.flush()
+                        properties["children"][fname.name] = find_file_properties_recursively(
+                            _tempfile.name, print_properties
+                        )
 
     return properties
 
