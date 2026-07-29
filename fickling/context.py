54897ecc1854f0e67b3083b31b002df8a43a8bf2
Thomas Chauchefoin <thomas.chauchefoin@trailofbits.com>
2026-07-29 18:53:32 +0200

    Drop dead max_acceptable_severity from FicklingContextManager
    The lambda meant to apply the threshold was assigned and discarded, so
    run_hook() has always installed loader.load with its own default. Remove
    the parameter rather than keep advertising an option that does nothing.

--- fickling/context.py
+++ fickling/context.py
@@ -1,20 +1,13 @@
 import pickle
 
 import fickling.hook as hook
-import fickling.loader as loader
-from fickling.analysis import Severity
 
 
 class FicklingContextManager:
-    def __init__(self, max_acceptable_severity=Severity.LIKELY_SAFE):
+    def __init__(self):
         self.original_pickle_load = pickle.load
-        self.max_acceptable_severity = max_acceptable_severity
 
     def __enter__(self):
-        # Modify the `hook_pickle_load` function to use the imported loader
-        wrapped_load = lambda file, *args, **kwargs: loader.load(  # noqa
-            file, max_acceptable_severity=self.max_acceptable_severity
-        )
         hook.run_hook()
         return self
 
